c410128e797dc6ebdaa3ed15b8eb1af495ed2c74
Thành Trang <14819342+trangcongthanh@users.noreply.github.com>
2024-06-14 13:22:26 +0700

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import cloudflare from "@astrojs/cloudflare";
+import markdoc from "@astrojs/markdoc";
+import react from "@astrojs/react";
+import keystatic from "@keystatic/astro";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), markdoc(), keystatic()],
+  output: "server",
+  adapter: cloudflare(),
+});
+
