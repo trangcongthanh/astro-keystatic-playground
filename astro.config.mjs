13ed2aeabcea36399c9ab59e32dbb286c338bd20
Thành Trang <14819342+trangcongthanh@users.noreply.github.com>
2024-06-14 14:28:13 +0700

    change to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
-  output: "server",
+  output: "hybrid",
   adapter: cloudflare(),
 });
 
